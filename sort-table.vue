78cfd5732814d5b76d33fae79f5ecf12e092f090
crazy0023 <124178927@qq.com>
2020-01-09 22:37:04 +0800

    s

---
+++ sort-table.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="tab-wrap">
+    <ElTable :data="data" :columns="columns"></ElTable>
+  </div>
+</template>
+<script>
+import ElTable from './table/el-table'
+export default {
+  components: {
+    ElTable
+  },
+  data() {
+    return {
+      columns: [
+        {
+          title: '姓名',
+          key: 'name'
+        },
+        {
+          title: '出生日期',
+          key: 'birthday'
+        },
+        {
+          title: '住址',
+          key: 'address'
+        },
+        {
+          title: '年龄',
+          key: 'age',
+          sortTable: true
+        }
+      ],
+      data: [
+        {
+          name: '俞小凡1',
+          age: 19,
+          birthday: '1999-02-21',
+          address: '北京市朝阳区芍瑶殿'
+        },
+        {
+          name: '俞小凡2',
+          age: 19,
+          birthday: '1999-02-21',
+          address: '北京市朝阳区芍瑶殿'
+        },
+        {
+          name: '俞小凡3',
+          age: 19,
+          birthday: '1999-02-21',
+          address: '北京市朝阳区芍瑶殿'
+        },
+        {
+          name: '俞小凡4',
+          age: 19,
+          birthday: '1999-02-21',
+          address: '北京市朝阳区芍瑶殿'
+        }
+      ]
+    }
+  }
+}
+</script>
+<style lang="less">
+// @import '//at.alicdn.com/t/font_1606356_28iws5teo36.css';
+</style>
---
+++ table/el-table.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="el-table">
+    <table>
+      <thead>
+        <tr>
+          <th v-for="item of currentColumns" :key="item._index">
+            <span>
+              {{item.title}}
+              <i class="el-paixu-shengxu"></i>
+            </span>
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item of currentData" :key="item.index">
+          <td v-for="col of currentColumns" :key="col.key">
+            <span>{{item[col.key]}}</span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    columns: {
+      type: Array,
+      default: () => {
+        return []
+      }
+    },
+    data: {
+      type: Array,
+      default: () => {
+        return []
+      }
+    }
+  },
+  data() {
+    return {
+      currentColumns: [],
+      currentData: []
+    }
+  },
+  methods: {
+    makeColumns() {
+      this.currentColumns = this.columns.map((col, index) => {
+        return {
+          ...col,
+          _sortType: 'normal',
+          _index: index
+        }
+      })
+    },
+    makeData() {
+      this.currentData = this.data.map((row, index) => {
+        return {
+          ...row,
+          _index: index
+        }
+      })
+    }
+  },
+  mounted() {
+    this.makeColumns()
+    this.makeData()
+  }
+}
+</script>
+<style lang="less">
+@import '//at.alicdn.com/t/font_1606356_28iws5teo36.css';
+[class^='el-'] {
+  font-family: iconfont !important;
+  font-style: normal;
+  font-weight: 400;
+  font-variant: normal;
+  text-transform: none;
+  line-height: 1;
+  vertical-align: baseline;
+  display: inline-block;
+
+  color: #606266;
+  margin: 0 6px;
+  font-size: 14px;
+  vertical-align: middle;
+  cursor: pointer;
+}
+</style>
